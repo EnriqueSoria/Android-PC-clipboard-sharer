359a17ebd9ecf0269d33b9f99cd986c99e27658b
nrikee <ensomag@inf.upv.es>
2014-10-02 12:16:53 +0200

    Commenting things

--- androidClipBoard.py
+++ androidClipBoard.py
@@ -1,20 +1,41 @@
 import androidhelper
+import socket
+
+'''
+	Android (client) side.
+
+	Uses QPython (2.x)
+'''
+
 
 droid = androidhelper.Android()
 
+# Let's know that IP
 TCP_IP = droid.dialogGetInput().result
 
-def sendText(text):
-	import socket
-	
-	TCP_PORT = 5005
-	BUFFER_SIZE = 1024
+def sendText(text, ip, port=5005, buffer_size=1024):
+	'''
+		Creates a socket and send the text
+	'''
+	# Constants
 	MESSAGE = text
+	TCP_IP = ip
+	TCP_PORT = port
+	BUFFER_SIZE = buffer_size
 	
+	# Create a socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Connect to IP:PORT
 	s.connect((TCP_IP, TCP_PORT))
+
+	# Send that message
 	s.send(MESSAGE)
+
+	# Get reply
 	data = s.recv(BUFFER_SIZE)
+
+	# Close socket
 	s.close()
 	
 
@@ -22,6 +43,6 @@ while(true):
 	clipboard = droid.getClipboard()
 	text = clipboard.text
 
-	sendText(text)
+	sendText(text, ip)
 	time.sleep(5)
 
--- androidClipboardReceiver.py
+++ androidClipboardReceiver.py
@@ -1,19 +1,38 @@
 import socket
 import time
 
+'''
+	PC (server) side.
+
+	It receives data, and puts it into the clipboard.
+'''
+
+def setTextToClipboard(text):
+	# TODO
+	
+	return False
+
+# Constants
 TCP_IP = '192.168.1.3'
 TCP_PORT = 6006
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024
 
+# We create a socket server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-
 while True:
+	# Accept connection
 	conn, addr = s.accept()
+
+	# Log addres
 	print 'Connection address:', addr
+
+	# Receive data
 	data = conn.recv(BUFFER_SIZE)
+
+	# Process data
 	if data:
 		print data
 		conn.send('ok')
