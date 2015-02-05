814ca8c753f0e942cf9ec24737defc8629fb0aac
Enrique Soria <EnriqueSoria@users.noreply.github.com>
2015-02-05 09:08:24 +0100

    Setting text to clipboard

--- androidClipboardReceiver.py
+++ androidClipboardReceiver.py
@@ -1,16 +1,27 @@
-import socket
-import time
-
 '''
 	PC (server) side.
 
 	It receives data, and puts it into the clipboard.
+	It needs: http://sourceforge.net/projects/pywin32/
 '''
+import socket
+import time
+import win32clipboard
 
-def setTextToClipboard(text):
-	# TODO
-	
-	return False
+def get_clipboard():
+	''' Opens a clipbard and get the text '''
+	win32clipboard.OpenClipboard()
+	data = win32clipboard.GetClipboardData()
+	win32clipboard.CloseClipboard()
+	return data
+
+def set_clipboard(text):
+	''' Opens a clipboard and sets text to it '''
+	win32clipboard.OpenClipboard()
+	win32clipboard.EmptyClipboard()
+	win32clipboard.SetClipboard(text.encode('utf-8'), win32clipboard.CF_TEXT)
+	win32clipboard.SetClipboard(unicode(text), win32clipboard.CF_UNICODETEXT)
+	win32clipboard.CloseClipboard()
 
 # Constants
 TCP_IP = '192.168.1.3'
@@ -25,15 +36,13 @@ s.listen(1)
 while True:
 	# Accept connection
 	conn, addr = s.accept()
-
 	# Log addres
 	print 'Connection address:', addr
-
 	# Receive data
 	data = conn.recv(BUFFER_SIZE)
-
 	# Process data
 	if data:
+		set_clipboard(data)
 		print data
 		conn.send('ok')
 		conn.close()
